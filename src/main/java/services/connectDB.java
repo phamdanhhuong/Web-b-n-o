58646018fdb150f152115b843c561905d739ab10
Pham Danh Huong <danhhuong246@gmail.com>
2024-11-30 23:25:43 +0700

    thêm gson-2.11.0.jar

--- src/main/java/services/connectDB.java
+++ src/main/java/services/connectDB.java
@@ -8,11 +8,11 @@ public class connectDB {
 	public static Connection DB () {
 		Connection con = null;
 		String username = "sa";
-        //String password = "123456789";
-        String password = "12345";
+        String password = "123456789";
+        //String password = "12345";
         String nameDatabase ="WebBanAo";
-        //String nameLap="MSI";
-        String nameLap="LAPTOP-PPR9IB31";
+        String nameLap="MSI";
+        //String nameLap="LAPTOP-PPR9IB31";
         String url = "jdbc:sqlserver://"+nameLap+":1433;databaseName="+nameDatabase+";encrypt=false";
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
